d8096225d73c4cd09bd191a2ea33c03cb7323b25
sanjita97 <sanjitasilpa1997@gmail.com>
2024-03-03 16:01:36 +0530

    code added

---
+++ src/org/git/DemoGitHub.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class DemoGitHub {
+
+	public static void main(String[] args) {
+			
+			System.out.println("This is git, where source code is pushed to the destination.");
+		}
+
+	}
+
+
